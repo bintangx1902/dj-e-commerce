03a86ae694f40c52860cdedd39e9caea677e2c8e
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-01-19 22:39:57 +0700

    core apps

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.FloatField()
+    discount = models.FloatField()
+    item_slug = models.SlugField(max_length=255, unique=True)
+    description = RichTextField
+    image = models.ImageField(upload_to='/img')
+
--- dj_commerce/settings.py
+++ dj_commerce/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'crispy_forms',
     'django_countries',
+
+    'core',
 ]
 
 SITE_ID = 1
