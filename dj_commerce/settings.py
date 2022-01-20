b93dd49766d3a65b47767205802271bb660c4be6
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-01-20 22:50:54 +0700

    registering templates to settings

--- dj_commerce/settings.py
+++ dj_commerce/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'dj_commerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
