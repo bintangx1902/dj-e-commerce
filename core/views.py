4b0694967329b96bf20358df1f54a04c168b2c4a
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-01-21 22:51:13 +0700

    item control

--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic import *
 from .models import Item
 
+item_slug = 'item_slug'
+
 
 class HomeLandingView(ListView):
     model = Item
@@ -14,7 +16,26 @@ class ItemDetailView(DetailView):
     model = Item
     template_name = ''
     query_pk_and_slug = True
-    slug_field = 'item_slug'
-    slug_url_kwarg = 'item_slug'
+    slug_field = item_slug
+    slug_url_kwarg = item_slug
 
 
+class CreateItemViews(CreateView):
+    model = Item
+    template_name = None
+    form_class = None
+
+    def get_success_url(self):
+        return
+
+
+class UpdateItemViews(UpdateView):
+    model = Item
+    query_pk_and_slug = True
+    slug_field = item_slug
+    slug_url_kwarg = item_slug
+    form_class = None
+    template_name = None
+
+    def get_success_url(self):
+        return
