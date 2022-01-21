74362d3499256227b81a304cd2b22f03f596dce3
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-01-22 00:19:35 +0700

    update create set

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,6 @@ app_name = 'core'
 urlpatterns = [
     path('', HomeLandingView.as_view(), name='home'),
     path('create-item', CreateItemViews.as_view(), name='item-create'),
-    path('<slug:item_slug>/update', UpdateItemViews.as_view(), name='item-detail'),
+    path('<slug:item_slug>/update', UpdateItemViews.as_view(), name='item-update'),
     path('<slug:item_slug>', ItemDetailView.as_view(), name='item-detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
 from django.views.generic import *
 from .forms import *
 
@@ -26,6 +26,7 @@ class HomeLandingView(ListView):
     model = Item
     paginate_by = 10
     ordering = ['-id']
+    context_object_name = 'items'
     template_name = 'core/item_list.html'
 
 
@@ -43,7 +44,7 @@ class CreateItemViews(CreateView):
     form_class = CreateItemForms
 
     def get_success_url(self):
-        return
+        return redirect('core:home')
 
     def form_valid(self, form):
         link: str = form.cleaned_data['title']
@@ -64,8 +65,13 @@ class UpdateItemViews(UpdateView):
     query_pk_and_slug = True
     slug_field = item_slug
     slug_url_kwarg = item_slug
-    form_class = None
-    template_name = None
+    form_class = UpdateItemForms
+    template_name = 'core/forms.html'
 
     def get_success_url(self):
-        return
+        return reverse('core:item-update', kwargs={'item_slug': self.kwargs['item_slug']})
+
+    def get_context_data(self, **kwargs):
+        context = super(UpdateItemViews, self).get_context_data(**kwargs)
+        context['title'] = 'Update'
+        return context
