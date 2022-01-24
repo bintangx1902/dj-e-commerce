f7422ec1f4dd8b32ee144f5bcc8e8116a65c7a75
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-01-24 22:36:11 +0700

    commerce views and template prepare

--- commerce/views.py
+++ commerce/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import *
+from core.models import Item
 
-# Create your views here.
+item_slug = 'item_slug'
+
+
+class HomeView(ListView):
+    model = Item
+    template_name = None
+    paginate_by = 50
+    ordering = ['-pk']
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = None
+    query_pk_and_slug = True
+    slug_field = item_slug
+    slug_url_kwarg = item_slug
+
+
