714df57fdac2aecded58ad644f9d7c00d7091ad4
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-01-26 00:09:33 +0700

    add to cart fixing

--- commerce/views.py
+++ commerce/views.py
@@ -27,17 +27,27 @@ class ItemDetailView(DetailView):
 @login_required(login_url='/accounts/login/')
 def add_to_cart(request, item_slug):
     item = get_object_or_404(Item, item_slug=item_slug)
-    order_item = OrderItem.objects.get_or_create(item=item)
+    order_item, created = OrderItem.objects.get_or_create(
+        item=item,
+        user=request.user,
+        ordered=False
+    )
+
     order_qs = Order.objects.filter(user=request.user, ordered=False)
     if order_qs.exists():
-        order = order_qs[0]
+        order = order_qs.first()
         if order.item.filter(item__item_slug=item.item_slug).exists():
             order_item.quantity += 1
             order_item.save()
         else:
             order.item.add(order_item)
     else:
-        order = Order.objects.create(user=request.user, ordered_date=timezone.now())
+        ordered_date = timezone.now()
+        order = Order.objects.create(user=request.user, ordered_date=ordered_date)
         order.item.add(order_item)
 
     return HttpResponseRedirect(reverse('com:item-detail', kwargs={'item_slug': item_slug}))
+
+
+def remove_from_cart(request, item_slug):
+    item = get_object_or_404(Item, item_slug=item_slug)
