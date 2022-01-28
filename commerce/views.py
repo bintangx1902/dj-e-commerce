5bfbeb51ff09e8e26cad774778a1dd67db257b85
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-01-28 15:13:21 +0700

    order summary

--- commerce/views.py
+++ commerce/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, reverse
 from django.views.generic import *
 from core.models import Item, OrderItem, Order
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.utils import timezone
 from django.http import HttpResponseRedirect
 from django.contrib import messages
@@ -22,6 +23,18 @@ class ItemDetailView(DetailView):
     slug_field = 'item_slug'
     slug_url_kwarg = 'item_slug'
     context_object_name = 'item'
+
+
+class OrderSummaryView(ListView):
+    model = Order
+    template_name = None
+
+    def get_queryset(self):
+        return Order.objects.get(user=self.request.user, ordered=False)
+
+    @method_decorator(login_required(login_url='accounts/login/'))
+    def dispatch(self, request, *args, **kwargs):
+        return super(OrderSummaryView, self).dispatch(request, *args, **kwargs)
 
 
 @login_required(login_url='/accounts/login/')
@@ -52,6 +65,7 @@ def add_to_cart(request, item_slug):
     return HttpResponseRedirect(reverse('com:item-detail', kwargs={'item_slug': item_slug}))
 
 
+@login_required(login_url='accounts/login/')
 def remove_from_cart(request, item_slug):
     item = get_object_or_404(Item, item_slug=item_slug)
     order_qs = Order.objects.filter(user=request.user, ordered=False)
