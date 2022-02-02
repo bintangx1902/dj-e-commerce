8f64e0e921c0f735450803816a7f83ac5a3d4c97
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-02-02 20:01:07 +0700

    stripe error handling

--- commerce/views.py
+++ commerce/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, reverse, render, redirect
 from django.views.generic import *
-from core.models import Item, OrderItem, Order, BillingAddress
+from core.models import Item, OrderItem, Order, BillingAddress, Payment
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.utils import timezone
@@ -94,14 +94,53 @@ class PaymentView(View):
 
     def post(self, *args, **kwargs):
         order = Order.objects.get(user=self.request.user, ordered=False)
-
         token = self.request.GET.get('stripeToken')
-        stripe.Charge.create(
-            amount=order.get_total(),
-            currency="idr",
-            source=token,
-        )
+        amount = order.get_total()
+
+        # error handling
+        try:
+            charge = stripe.Charge.create(
+                amount=amount,
+                currency="idr",
+                source=token,
+            )
+
+        except stripe.error.CardError as e:
+            # Since it's a decline, stripe.error.CardError will be caught
+
+            print('Status is: %s' % e.http_status)
+            print('Code is: %s' % e.code)
+            # param is '' in this case
+            print('Param is: %s' % e.param)
+            print('Message is: %s' % e.user_message)
+        except stripe.error.RateLimitError as e:
+            # Too many requests made to the API too quickly
+            pass
+        except stripe.error.InvalidRequestError as e:
+            # Invalid parameters were supplied to Stripe's API
+            pass
+        except stripe.error.AuthenticationError as e:
+            # Authentication with Stripe's API failed
+            # (maybe you changed API keys recently)
+            pass
+        except stripe.error.APIConnectionError as e:
+            # Network communication with Stripe failed
+            pass
+        except stripe.error.StripeError as e:
+            # Display a very generic error to the user, and maybe send
+            # yourself an email
+            pass
+        except Exception as e:
+            # Something else happened, completely unrelated to Stripe
+            pass
+        # end handling
+
         order.ordered = True
+        payment = Payment()
+        payment.stripe_charge_id = charge.id
+        payment.user = self.request.user
+        payment.amount = amount
+        payment.save()
 
 
 @login_required(login_url='/accounts/login/')
